35a60caff84e072b0e30ad8c6f25af0153239fef
nikhil-eashy <nikhileashy74@gmail.com>
2023-03-07 01:59:16 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// const mult =(a,b)=>{
+//     console.log(a*b)
+// }
+// let x =10;
+// let y =20;
+// mult(x,y)
+
+
+// let z= 30;
+// let infos = [
+//     {name:"Nikhil",place:"kallur",age:28},
+//     {name:"Muhsina",place:"muttichoor",age:90}
+// ]
+// console.log(`${infos[0].name} and ${infos[1].name}`);
+//console.log(person.name + " " + person.place)
+// console.log("Sum =",sum);
+// console.log("sub =",sub);
+// console.log(m);
+// console.log(++d);
+function p(x)
+{
+    console.log(x)
+}
+
+let fruits = ["apple","bannana","orangle"]
+let i =0;
+
+do{
+    p(fruits[i]);
+    i++;
+}while(i<fruits.length)
+
+// for(let i=0;i<fruits.length;i++)
+// {
+//     p(fruits[i])
+// }
